49c1a7b50ef32549ba1234c1f759bee1e4ef3adc
Nestor <nestorgamarradb@unimagdalena.edu.co>
2023-01-21 14:04:48 -0500

    dokcker_compose

--- scrip.py
+++ scrip.py
@@ -3,7 +3,7 @@ import time
 from influxdb_client.client.write_api import SYNCHRONOUS
 from influxdb_client import InfluxDBClient, Point
 
-token = '71KxhK1XFYObAEMnECTiHDTCmUfWu5Pk954mEx7XGQ0PbZsjPvVQDvdX4o8qorT2iaQXayaf6J11fN1w3td79w=='
+token = 'nGf_e-6LihQR2AqEFbD5Bhc0iFlcAl8WeD6SlOgG883Fgmroau4fhBie5Ydzzij72UqtManqpm0MlxjZ2FMlXw=='
 bucket = 'test'
 client = InfluxDBClient('http://localhost:8086',token = token, org='test')
 
@@ -12,34 +12,28 @@ query_api = client.query_api()
 
 
 while True:
- city = "bogota"
- def return_weather(city):
-    url = f"https://es.wttr.in/{city}?format=j1"
+    city = "bogota"
+    def return_weather(city):
+      url = "https://es.wttr.in/{city}?format=j1"
 
-    response = requests.get(url)
-    weather_dic = response.json()
+      response = requests.get(url)
+      weather_dic = response.json()
 
-    temp_c = weather_dic["current_condition"][0]['temp_C']
-    humedad= weather_dic["current_condition"][0]['humidity']
-    return temp_c,humedad
+      temp_c = weather_dic["current_condition"][0]['temp_C']
+      humedad= weather_dic["current_condition"][0]['humidity']
+      return temp_c,humedad
         
- def main():
-    temp_c,humedad = return_weather(city)
+    def main():
+       temp_c,humedad = return_weather(city)
     
-    if    temp_c !=0 and   humedad !=0:
-        print(f"La temperatura actual de {city} es {temp_c} °C.")
-        print(f"La humedad actual de {city} es {humedad} %. .")
+       if    temp_c !=0 and   humedad !=0:
+           print(f"La temperatura actual de {city} es {temp_c} °C.")
+           print(f"La humedad actual de {city} es {humedad} %. .")
 
-        p = Point("Datos").tag("Ciudad", city).field("temperatura", float(temp_c)).field('humedad', float(humedad))
-        write_api.write(bucket=bucket, org='test', record=p)
+           p = Point("Datos").field("temperatura", float(temp_c)).field('humedad', float(humedad))
+           write_api.write(bucket=bucket, org='test', record=p)
 
- time.sleep(1)
+    time.sleep(1)
  
- if __name__ == '__main__':
-    main()
-    
-
-
-
-
-
+    if __name__ == '__main__':
+        main()
